5d6531dfa00fe5697467536371ae19f0d7c2736b
sunnymayahn <sunnysunnytown@gamil.com>
2021-09-17 13:14:33 +0900

    917

---
+++ 04company_teacher/js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    const slider1 = $(".slider").bxSlider({
+        controls:false
+    });
+    $(".sliderWrap .nextBtn").click(function(){
+        slider1.goToNextSlide();
+    });
+    $(".sliderWrap .prevBtn").click(function(){
+        slider1.goToPrevSlide();
+    });
+
+    const slider2 = $(".slider2").bxSlider({
+        controls:false,
+        pager:false,
+        auto:true
+    });
+
+    $(".slider2Wrap .nextBtn").click(function(){
+        slider2.goToNextSlide();
+    });
+    $(".slider2Wrap .prevBtn").click(function(){
+        slider2.goToPrevSlide();
+    });
+
+    // $(".toggle").on("이벤트명",리스너);
+    // $(".toggle").on("click",function(){});
+    // $(".toggle").click(function(){})
+    $(".toggle").click(function(){
+        const toggle = $(".toggle").attr("class"); // value toggle
+        console.log(toggle);
+
+        if( toggle == "toggle"){
+            //$(".toggle").addClass("active");
+            document.getElementsByClassName("toggle")[0].classList.add("active");
+            $(".gnb").addClass("view");
+        }else{
+            //$(".toggle").removeClass("active");
+            document.getElementsByClassName("toggle")[0].classList.remove("active");
+            $(".gnb").removeClass("view");
+        }
+    });
+
+    $(window).resize(function(){
+        $(".toggle").removeClass("active");
+        $(".gnb").removeClass("view");
+    });
+   
+});
